9e42346fe652d3e4cba03c63b145b02310ac7734
Pol <pol.roca.novellas@estudiantat.upc.edu>
2024-03-16 13:06:05 +0100

    Primera versio jeje

---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.MapProviders;
+using System.Drawing;
+
+//namespace FlightTracker
+//{
+//    public partial class Form1 : Form
+//    {
+//        GMapOverlay planeOverlay;
+
+//        public Form1()
+//        {
+//            InitializeComponent();
+//        }
+
+//        private void Form1_Load(object sender, EventArgs e)
+//        {
+//            // Configurar el control GMapControl
+//            gMapControl1.MapProvider = GMapProviders.GoogleMap;
+//            gMapControl1.Position = new PointLatLng(40.712776, -74.005974); // Establecer la posición inicial del mapa (Nueva York, por ejemplo)
+//            gMapControl1.MinZoom = 0;
+//            gMapControl1.MaxZoom = 24;
+//            gMapControl1.Zoom = 10;
+//            gMapControl1.DragButton = MouseButtons.Left;
+
+//            // Crear una capa para la trayectoria del avión
+//            planeOverlay = new GMapOverlay("plane");
+//            gMapControl1.Overlays.Add(planeOverlay);
+//        }
+
+//        private void DrawFlightPath(PointLatLng[] points)
+//        {
+//            // Crear una polilínea para representar la trayectoria del avión
+//            GMapRoute route = new GMapRoute(points, "FlightPath")
+//            {
+//                Stroke = new Pen(Color.Red, 2)
+//            };
+
+//            // Agregar la polilínea a la capa de la trayectoria del avión
+//            planeOverlay.Routes.Add(route);
+//        }
+
+//        // Ejemplo de cómo utilizar la función DrawFlightPath
+//        private void btnDrawPath_Click(object sender, EventArgs e)
+//        {
+//            // Supongamos que tenemos una lista de puntos que representan la trayectoria del avión
+//            PointLatLng[] flightPoints = new PointLatLng[]
+//            {
+//                new PointLatLng(40.712776, -74.005974), // Punto de partida
+//                new PointLatLng(34.052235, -118.243683), // Punto intermedio
+//                new PointLatLng(37.774929, -122.419416) // Punto de destino
+//            };
+
+//            // Dibujar la trayectoria del avión en el mapa
+//            DrawFlightPath(flightPoints);
+//        }
+//    }
+//}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+
+class Program
+{
+    static void Main()
+    {
+
+        byte[] fileBytes = ReadFile();
+
+        List<byte[]> Messages = GetCat48Messages(fileBytes);
+
+        for (int i = 0; i < Messages.Count; i++)
+        {
+            List<byte> fspec = Fspec(Messages[i]);
+
+            if(i == 5001)
+            {
+                Console.WriteLine(string.Join(",", fspec));
+            }
+        }
+
+        //Console.WriteLine(string.Join(",",Messages[5001]));
+    }
+
+    static List<byte[]> GetCat48Messages(byte[] fileBytes)
+    {
+        List<byte[]> listabyte = new List<byte[]>();
+        int i = 0;
+        int contador = getdeclen(fileBytes[1], fileBytes[2]);
+        //int contador = fileBytes[2];
+
+        while (i < fileBytes.Length)
+        {
+            byte[] array = new byte[contador];
+            for (int j = 0; j < array.Length; j++)
+            {
+                array[j] = fileBytes[i];
+                i++;
+            }
+            listabyte.Add(array);
+            if (i + 2 < fileBytes.Length)
+            {
+                contador = getdeclen(fileBytes[i + 1], fileBytes[i + 2]);
+                //contador = fileBytes[i + 2];
+            }
+        }
+        return listabyte;
+    }
+
+    static int getdeclen(byte l1, byte l2)
+    {
+        int Length = (l1 << 8) | l2;
+        return Length;
+    }
+
+    static byte[] ReadFile()
+    {
+        byte[] fileBytes = File.ReadAllBytes(@"C:\Users\polro\Desktop\PGTA\230502-est-080001_BCN_60MN_08_09.ast");
+        return fileBytes;
+    }
+
+    static List<byte> Fspec(byte[] message)
+    {
+        List<byte> fspec = new List<byte>();
+        int i = 3;
+        while(i != 0)
+        {
+            fspec.Add(message[i]);
+            if ((message[i] & 0b00000001) == 1) { i++; }
+            else { i = 0; }
+        }
+        return fspec;
+    }
+}
